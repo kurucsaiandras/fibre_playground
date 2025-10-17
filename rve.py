09e91bbc3b27167181ec6fc4f3758529dc796112
Kurucsai <kurucsaiandris@gmail.com>
2025-10-17 18:54:58 +0200

    collision line loss

--- rve.py
+++ rve.py
@@ -73,13 +73,66 @@ class RVE:
 
     def get_fibre_to_volume_ratio(self):
         return torch.pow(self.fibre_r, 2).sum() * math.pi / (self.domain_size[0] * self.domain_size[1])
+    
+    def collision_line_loss(self):
+        boxes = utils.get_bounding_boxes(self.fibre_coords, self.fibre_r)
+        intersections = utils.get_bbox_intersections(boxes, self.domain_size, self.apply_pbc)
+
+        i_idx, j_idx = intersections["normal"].nonzero(as_tuple=True)
+
+        d_i = self.fibre_coords[i_idx, 1:, :] - self.fibre_coords[i_idx, :-1, :]  # (n_ij, res-1, 3)
+        d_j = self.fibre_coords[j_idx, 1:, :] - self.fibre_coords[j_idx, :-1, :]  # (n_ij, res-1, 3)
+
+        a = (d_i * d_i).sum(dim=2)  # (n_ij, res-1)
+        e = (d_j * d_j).sum(dim=2)  # (n_ij, res-1)
+        b = (d_i[:, :, None, :] * d_j[:, None, :, :]).sum(dim=3)  # (n_ij, res-1, res-1)
+
+        r_ij = self.fibre_coords[i_idx, :-1, None, :] - self.fibre_coords[j_idx, None, :-1, :]  # (n_ij, res-1, res-1, 3)
+        c = (d_i[:, :, None, :] * r_ij).sum(dim=-1)  # (n_ij, res-1, res-1)
+        f = (d_j[:, :, None, :] * r_ij).sum(dim=-1)  # (n_ij, res-1, res-1)
+
+        denom = (a * e)[:, :, None] - b * b  # (n_ij, res-1, res-1)
+        denom_safe = denom.clone()
+        denom_safe[denom_safe.abs() < 1e-8] = 1.0  # avoid division by 0 for parallel lines
+
+        # Initial s and t
+        s = torch.clamp((b * f - c * e[:, :, None]) / denom_safe, 0.0, 1.0)
+        s[denom.abs() < 1e-8] = 0.0  # parallel lines
+        t = (b * s + f) / e[:, :, None]
+
+        # Clamp t and recompute s accordingly
+        t_lt0 = t < 0.0
+        t_gt1 = t > 1.0
+        t = torch.clamp(t, 0.0, 1.0)
+
+        # recompute s where t was clamped
+        s_new_lt0 = torch.clamp(-c / a[:, :, None], 0.0, 1.0)
+        s_new_gt1 = torch.clamp((b - c) / a[:, :, None], 0.0, 1.0)
+        s = torch.where(t_lt0, s_new_lt0, s)
+        s = torch.where(t_gt1, s_new_gt1, s)
+
+        # --- at this point, s and t are correctly clamped and recomputed ---
+
+        # Compute closest points (optional, depending on what loss uses)
+        p_i = self.fibre_coords[i_idx, :-1, None, :] + s[..., None] * d_i[:, :, None, :]
+        p_j = self.fibre_coords[j_idx, None, :-1, :] + t[..., None] * d_j[:, None, :, :]
+
+        dists = torch.linalg.norm(p_i - p_j, dim=-1)
+        expected_dists = self.fibre_r[i_idx].view(-1, 1, 1) + self.fibre_r[j_idx].view(-1, 1, 1)
+
+        # penalty
+        d_l = F.relu(expected_dists - dists)
+        penalties = 0.5 * self.k_collision * d_l*d_l
+
+        loss = penalties.sum()
+        return loss
 
     def collision_loss(self):
         boxes = utils.get_bounding_boxes(self.fibre_coords, self.fibre_r)
         intersections = utils.get_bbox_intersections(boxes, self.domain_size, self.apply_pbc)
 
         i_idx, j_idx = intersections["normal"].nonzero(as_tuple=True)
-        dists = torch.norm(self.fibre_coords[i_idx, :, None, :] - self.fibre_coords[j_idx, None, :, :], dim=-1)
+        dists = torch.norm(self.fibre_coords[i_idx, :, None, :] - self.fibre_coords[j_idx, None, :, :], dim=-1) # (n_ij, res, res)
         expected_dists = self.fibre_r[i_idx].view(-1,1,1) + self.fibre_r[j_idx].view(-1,1,1)
 
         if self.apply_pbc:
